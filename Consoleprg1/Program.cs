64d229e89e38301100b37078eaedd2949315ca8c
kavimihiranga20 <kmvithanage48@gmail.com>
2021-05-10 11:41:37 +0530

    Frist commit

---
+++ Consoleprg1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Consoleprg1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student student1 = new student();
+            student1.setstudent_id(219899);
+            Console.WriteLine(student1.getstudent_id());
+
+            student1.setstudent_name("kavindu");
+            Console.WriteLine(student1.getstudent_name());
+              
+        }
+
+        class student
+        {
+
+            private  int student_id;
+            private string student_name;
+
+            public void setstudent_id(int id) 
+            {
+                student_id = id;
+
+            }
+
+            public int getstudent_id()
+            {
+                return student_id;
+
+            
+            }
+
+            public void setstudent_name(string Name)
+            {
+
+                student_name = Name;
+            }
+
+            public string getstudent_name()
+            {
+
+                return student_name;
+            }
+
+        }
+
+    }
+}
